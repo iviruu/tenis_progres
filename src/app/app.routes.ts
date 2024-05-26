511f34042072dfc44a13878fd457811da21065f3
iviruu <ivan.kok.ru@gmail.com>
2024-05-27 01:36:28 +0200

    feat: Add router links to auth and registr components
    This commit adds the necessary imports for the RouterLink directive in the auth, home, and registr components. It also updates the app.routes.ts file to include the login and register paths. Additionally, the auth component template is updated to include a login form with email and password inputs.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { ProfeComponent } from './components/profe/profe.component';
 import { HomeComponent } from './components/home/home.component';
 import { AlumnoComponent } from './components/alumno/alumno.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { RegistrComponent } from './components/registr/registr.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'profe', component: ProfeComponent},
     {path: 'alumno', component: AlumnoComponent},
+    {path: 'login', component: AuthComponent},
+    {path: 'register', component: RegistrComponent},
     {path: '**', redirectTo: 'home'}
 ];
--- src/app/components/registr/registr.component.ts
+++ src/app/components/registr/registr.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registr',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './registr.component.html',
   styleUrl: './registr.component.css'
 })
