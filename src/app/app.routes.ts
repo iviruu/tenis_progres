84775a306ae7aa894854f4265cc9f962b3c6a131
iviruu <ivan.kok.ru@gmail.com>
2024-05-28 15:54:57 +0200

    chore: Remove Home2Component and related files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { HomeComponent } from './components/home/home.component';
 import { AlumnoComponent } from './components/alumno/alumno.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { RegistrComponent } from './components/registr/registr.component';
-import { Home2Component } from './components/home2/home2.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
@@ -12,6 +11,5 @@ export const routes: Routes = [
     {path: 'alumno', component: AlumnoComponent},
     {path: 'login', component: AuthComponent},
     {path: 'register', component: RegistrComponent},
-    {path: 'home2', component: Home2Component},
     {path: '**', redirectTo: 'home'}
 ];
