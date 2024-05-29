a9ec2ce0d5c4047926f32bdfd3af2ce8c8035ae0
iviruu <ivan.kok.ru@gmail.com>
2024-05-29 13:34:41 +0200

    chore: Update navbar component and router configuration
    This commit modifies the navbar component by adding the RouterLink import and updates the router configuration in the app.routes.ts file to include child routes for the profe component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,18 @@ import { HomeComponent } from './components/home/home.component';
 import { AlumnoComponent } from './components/alumno/alumno.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { RegistrComponent } from './components/registr/registr.component';
+import { PerfilComponent } from './shared/perfil/perfil.component';
+import { ListaAlumnosComponent } from './components/profe/lista-alumnos/lista-alumnos.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
-    {path: 'profe', component: ProfeComponent},
+    {path: 'profe', component: ProfeComponent, children: [
+        { path: '', redirectTo: 'perfil', pathMatch: 'full' },
+        { path: 'perfil', component: PerfilComponent },
+        { path: 'lista_alumnos', component: ListaAlumnosComponent },
+    ]},
     {path: 'alumno', component: AlumnoComponent},
     {path: 'login', component: AuthComponent},
     {path: 'register', component: RegistrComponent},
-    {path: '**', redirectTo: 'home'}
+    
 ];
--- src/app/components/profe/profe.component.ts
+++ src/app/components/profe/profe.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-profe',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './profe.component.html',
   styleUrl: './profe.component.css'
 })
