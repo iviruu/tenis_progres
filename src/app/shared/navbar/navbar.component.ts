a34b8359d2e5dcaae3e9a21ddc4b645a70ea1122
iviruu <ivan.kok.ru@gmail.com>
2024-05-29 11:16:10 +0200

    modificar navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent{
+
+
+
+
 
 }
